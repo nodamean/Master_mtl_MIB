aba2e6808b3d7854e4b0b8067aec61fa65c2e35d
nodamean <nanthanit.mean@gmail.com>
2015-06-16 16:40:38 +0700

    Add MTL_ForCDDScanEntity

---
+++ mtl_MIB/Memberships/Register.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Security;
+using System.Web.Security;
+using System.Web.Profile;
+using System.Data;
+using System.Data.Entity;
+namespace mtl_MIB.Memberships
+{
+    public partial class Register : System.Web.UI.Page
+    {
+       	
+        protected void CreateUser_Click(object sender, EventArgs e)
+        {
+            MembershipCreateStatus result;
+            try
+            {
+                MembershipUser newUser = Membership.CreateUser(
+                  txtUserName.Text,
+                  txtPassword.Text
+                  );
+               
+                using (var dbContext = new  MTL_ForCDDScanEntities()){
+                   
+                }
+
+            }
+            catch (MembershipCreateUserException ex)
+            {
+                Response.Write(ex.StatusCode);
+            }
+            catch (HttpException ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+    }
+}
