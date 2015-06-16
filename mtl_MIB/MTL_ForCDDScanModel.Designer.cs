3c900f6ed10c7d29c9db3f773b01729e2ba2b4b5
nodamean <nanthanit.mean@gmail.com>
2015-06-16 16:34:06 +0700

    Add MTL_ForCDDScanEntity

--- mtl_MIB/MTL_ForCDDScanModel.Designer.cs
+++ mtl_MIB/MTL_ForCDDScanModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\mtl87022\Source\Repos\mtl_MIB\mtl_MIB\App_Code\MTL_ForCDDScan.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\mtl87022\Source\Repos\mtl_MIB\mtl_MIB\MTL_ForCDDScanModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
